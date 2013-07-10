4ed180c6db43ebc7c4035102fd01b81e6f337829
ronl <renli_98@yahoo.com>
2013-07-10 19:55:19 +0000

    update Read from file index.html

--- web.js
+++ web.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var fs=require('fs');
-var buf = new buffer(50);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-   buf = fs.readFileSync('index.html');
+   var buf = fs.readFileSync('index.html');
 //  response.send('Hello World2!');
   response.send (buf.toString());
 });
