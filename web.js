aee080e302d392d419ca86edca04696b5517b99d
ronl <renli_98@yahoo.com>
2013-07-10 19:41:48 +0000

    Read from file index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs=require('fs');
+var buf = new buffer(50);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+   buf = fs.readFileSync('index.html');
+//  response.send('Hello World2!');
+  response.send (buf.toString());
 });
 
 var port = process.env.PORT || 5000;
