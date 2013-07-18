9f159aa95d22737e5785bfc6845e2930fce17ce1
Ubuntu <ubuntu@ip-172-31-31-182.us-west-2.compute.internal>
2013-07-18 05:53:17 +0000

    add css styple

--- web.js
+++ web.js
@@ -1,3 +1,4 @@
+#!/uer/bin/env node
 var express = require('express');
 var fs=require('fs');
 
@@ -9,7 +10,7 @@ app.get('/', function(request, response) {
   response.send (buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
